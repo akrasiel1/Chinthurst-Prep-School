9885f69c485bcaab6ebc777d514e8d9719626d24
Antonio Monda <41501786+akrasiel1@users.noreply.github.com>
2019-06-14 06:46:28 +0100

    testimonials and social section implemented

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $( document ).ready(function() {
+
     
     // SET BROWSER
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
+
+    //SET LIGHTSLIDER
+    $("#testimonials").lightSlider({
+        item: 1,
+        loop: true,
+        speed: 750,
+        pauseOnHover: true,
+        mode: 'slide',
+        auto: true,
+        pause: 8000
+    });
+
 
 });
